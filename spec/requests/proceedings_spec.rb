9441caa4bf910a915e2884234192dcee5d96580a
Gegardus <stgegard@gmail.com>
2022-06-05 16:09:07 +0400

    Fix lynters

--- spec/requests/proceedings_spec.rb
+++ spec/requests/proceedings_spec.rb
@@ -12,94 +12,91 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/proceedings", type: :request do
-  
+RSpec.describe '/proceedings', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Proceeding. As you add validations to Proceeding, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Proceeding.create! valid_attributes
       get proceedings_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       proceeding = Proceeding.create! valid_attributes
       get proceeding_url(proceeding)
       expect(response).to be_successful
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
+  describe 'GET /new' do
+    it 'renders a successful response' do
       get new_proceeding_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /edit" do
-    it "renders a successful response" do
+  describe 'GET /edit' do
+    it 'renders a successful response' do
       proceeding = Proceeding.create! valid_attributes
       get edit_proceeding_url(proceeding)
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Proceeding" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Proceeding' do
+        expect do
           post proceedings_url, params: { proceeding: valid_attributes }
-        }.to change(Proceeding, :count).by(1)
+        end.to change(Proceeding, :count).by(1)
       end
 
-      it "redirects to the created proceeding" do
+      it 'redirects to the created proceeding' do
         post proceedings_url, params: { proceeding: valid_attributes }
         expect(response).to redirect_to(proceeding_url(Proceeding.last))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Proceeding" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Proceeding' do
+        expect do
           post proceedings_url, params: { proceeding: invalid_attributes }
-        }.to change(Proceeding, :count).by(0)
+        end.to change(Proceeding, :count).by(0)
       end
 
-    
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post proceedings_url, params: { proceeding: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        skip('Add a hash of attributes valid for your model')
+      end
 
-      it "updates the requested proceeding" do
+      it 'updates the requested proceeding' do
         proceeding = Proceeding.create! valid_attributes
         patch proceeding_url(proceeding), params: { proceeding: new_attributes }
         proceeding.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "redirects to the proceeding" do
+      it 'redirects to the proceeding' do
         proceeding = Proceeding.create! valid_attributes
         patch proceeding_url(proceeding), params: { proceeding: new_attributes }
         proceeding.reload
@@ -107,26 +104,24 @@ RSpec.describe "/proceedings", type: :request do
       end
     end
 
-    context "with invalid parameters" do
-    
+    context 'with invalid parameters' do
       it "renders a response with 422 status (i.e. to display the 'edit' template)" do
         proceeding = Proceeding.create! valid_attributes
         patch proceeding_url(proceeding), params: { proceeding: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested proceeding" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested proceeding' do
       proceeding = Proceeding.create! valid_attributes
-      expect {
+      expect do
         delete proceeding_url(proceeding)
-      }.to change(Proceeding, :count).by(-1)
+      end.to change(Proceeding, :count).by(-1)
     end
 
-    it "redirects to the proceedings list" do
+    it 'redirects to the proceedings list' do
       proceeding = Proceeding.create! valid_attributes
       delete proceeding_url(proceeding)
       expect(response).to redirect_to(proceedings_url)
--- spec/routing/proceedings_routing_spec.rb
+++ spec/routing/proceedings_routing_spec.rb
@@ -1,38 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ProceedingsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/proceedings").to route_to("proceedings#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/proceedings').to route_to('proceedings#index')
     end
 
-    it "routes to #new" do
-      expect(get: "/proceedings/new").to route_to("proceedings#new")
+    it 'routes to #new' do
+      expect(get: '/proceedings/new').to route_to('proceedings#new')
     end
 
-    it "routes to #show" do
-      expect(get: "/proceedings/1").to route_to("proceedings#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/proceedings/1').to route_to('proceedings#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(get: "/proceedings/1/edit").to route_to("proceedings#edit", id: "1")
+    it 'routes to #edit' do
+      expect(get: '/proceedings/1/edit').to route_to('proceedings#edit', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(post: "/proceedings").to route_to("proceedings#create")
+    it 'routes to #create' do
+      expect(post: '/proceedings').to route_to('proceedings#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/proceedings/1").to route_to("proceedings#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/proceedings/1').to route_to('proceedings#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/proceedings/1").to route_to("proceedings#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/proceedings/1').to route_to('proceedings#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/proceedings/1").to route_to("proceedings#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/proceedings/1').to route_to('proceedings#destroy', id: '1')
     end
   end
 end
