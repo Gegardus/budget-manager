156d16e7b46c32535c9bdfb7e348290f3f7120d3
Gegardus <stgegard@gmail.com>
2022-06-07 12:05:01 +0400

    Update views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,18 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+window.addEventListener('load', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const menu = document.getElementById('mobile-menu');
+    const backdrop = document.getElementById('backdrop');
+    hamburgerBtn.addEventListener('click', () => {
+      menu.classList.remove('-translate-x-full');
+      backdrop.classList.remove('hidden');
+    });
+  
+    backdrop.addEventListener('click', () => {
+      menu.classList.add('-translate-x-full');
+      backdrop.classList.add('hidden');
+    });
+  });
+  
